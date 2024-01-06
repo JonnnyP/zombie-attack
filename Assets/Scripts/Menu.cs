a1978dac42e6560495b1d0e80b7eb20fdb90d44c
JonnnyP <jonathan_plata@ymail.com>
2024-01-05 18:44:07 -0800

    main menu added

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+    public void OnPlayButton() {
+
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnQuitButton() {
+
+        Application.Quit();
+    }
+
+}
